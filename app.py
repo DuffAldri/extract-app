7a4573aa65bfc2b433811a345894ff2d7079833b
Muhammad Daffa Aldriantama <mdaffaaldriantama@gmail.com>
2024-07-24 09:30:03 +0700

    Perbaiki predictions.html

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ app.register_blueprint(main_bp)
 app.register_blueprint(prediction_bp)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    app.run(host='0.0.0.0', port='8080')
